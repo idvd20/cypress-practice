a7dccbbd528c886055566447de7b52319ad4ab46
idvd20 <65553817+idvd20@users.noreply.github.com>
2023-11-14 21:36:53 +0800

    accordion component test

--- cypress/component/Accordion.cy.jsx
+++ cypress/component/Accordion.cy.jsx
@@ -1,5 +1,38 @@
+const { default: ItemsAccordion } = require("@/app/components/Accordion");
+
+const items = [
+	{
+		id: 1,
+		summary: "Summary 1",
+		details: "Details 1",
+	},
+	{
+		id: 2,
+		summary: "Summary 2",
+		details: "Details 2",
+	},
+	{
+		id: 3,
+		summary: "Summary 3",
+		details: "Details 3",
+	},
+];
+
 describe("Accordion.cy.jsx", () => {
-	it("playground", () => {
-		// cy.mount()
+	it("should test Items Accordion", () => {
+		cy.mount(<ItemsAccordion items={items} />);
+		cy.getDataTest("accordion-wrapper").within(() => {
+			cy.get('[data-test^="accordion-item"]').should("have.length", 3);
+		});
+
+		cy.contains(/Details 1/i).should("not.be.visible");
+		cy.getDataTest("accordion-item-1").within(() => {
+			cy.get('div[role="button"]').click();
+		});
+		cy.contains(/Details 1/i).should("be.visible");
+		cy.getDataTest("accordion-item-1").within(() => {
+			cy.get('div[role="button"]').click();
+		});
+		cy.contains(/Details 1/i).should("not.be.visible");
 	});
 });
